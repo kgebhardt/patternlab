82beb4c8bd104c7219a41524dbd75b576ae67c9d
Kerstin Flederer <kerstin.flederer@mayflower.de>
2013-08-02 10:31:32 +0200

    * add some colors * changed styleguide: width of color boxes

--- patterns/00-Atoms/00-Global/00-Colors.php
+++ patterns/00-Atoms/00-Global/00-Colors.php
@@ -1,38 +1,47 @@
 <ul class="sg-colors">
 	<li>
-		<span class="sg-swatch" style="background: #ff0000;"></span>
-		<span class="sg-label">#ff0000</span>
+		<span class="sg-swatch" style="background: #4c3e3d;"></span>
+		<span class="sg-variable">$color-font</span>
+		<span class="sg-label">#4c3e3d</span>
 	</li>
 	<li>
-		<span class="sg-swatch" style="background: #00ff00;"></span>
-		<span class="sg-label">#00ff00</span>
+		<span class="sg-swatch" style="background: #715E4D;"></span>
+		<span class="sg-variable">$color-headline</span>
+		<span class="sg-label">#715E4D</span>
 	</li>
 	<li>
-		<span class="sg-swatch" style="background: #0000ff;"></span>
-		<span class="sg-label">#0000ff</span>
+		<span class="sg-swatch" style="background: #5F4F69;"></span>
+		<span class="sg-variable">$color-link</span>
+		<span class="sg-label">#5F4F69</span>
 	</li>
 	<li>
-		<span class="sg-swatch" style="background: #ffff00;"></span>
-		<span class="sg-label">#ffff00</span>
+		<span class="sg-swatch" style="background: #A5966F;"></span>
+		<span class="sg-variable">$color-border</span>
+		<span class="sg-label">#A5966F</span>
 	</li>
 	<li>
-		<span class="sg-swatch" style="background: #00ffff;"></span>
-		<span class="sg-label">#00ffff</span>
+		<span class="sg-swatch" style="background: #FFFD93;"></span>
+		<span class="sg-variable">$bg-color-button</span>
+		<span class="sg-label">#FFFD93</span>
 	</li>
 	<li>
-		<span class="sg-swatch" style="background: #ff00ff;"></span>
-		<span class="sg-label">#ff00ff</span>
+		<span class="sg-swatch" style="background: #D1C078;"></span>
+		<span class="sg-variable">$bg-color-button-hover</span>
+		<span class="sg-label">#D1C078</span>
 	</li>
 	<li>
-		<span class="sg-swatch" style="background: #ffffff;"></span>
-		<span class="sg-label">#ffffff</span>
+		<span class="sg-swatch" style="background: #505D75;"></span>
+		<span class="sg-variable">$bg-color-button-dark</span>
+		<span class="sg-label">#505D75</span>
 	</li>
 	<li>
-		<span class="sg-swatch" style="background: #808080;"></span>
-		<span class="sg-label">#808080</span>
+		<span class="sg-swatch" style="background: #87302E;"></span>
+		<span class="sg-variable">$error</span>
+		<span class="sg-label">#87302E</span>
 	</li>
 	<li>
-		<span class="sg-swatch" style="background: #000000;"></span>
-		<span class="sg-label">#000000</span>
+		<span class="sg-swatch" style="background: #8EB838;"></span>
+		<span class="sg-variable">$valid</span>
+		<span class="sg-label">#8EB838</span>
 	</li>
 </ul>
